bb9b3220ed0db09ab145b1266bda47513b3f1aab
adamben04 <adambenazouz@computingforall.org>
2023-05-16 02:35:32 -0700

    initialize project

---
+++ src/application/Main.java
@@ -0,0 +1,30 @@
+package application;
+
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+import javafx.scene.paint.Color;
+
+public class Main extends Application {
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        Group root = new Group();
+        Scene scene = new Scene(root,Color.BLACK);
+
+        Image icon = new Image("icon.png");
+        stage.getIcons().add(icon);
+        stage.setTitle("Algorithm Visualizer");
+
+        stage.setScene(scene);
+        stage.show();
+    }
+}
